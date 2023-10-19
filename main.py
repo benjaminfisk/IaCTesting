1405a5e45aa8af8f65d1ced5956adc951625bda6
Benjamin Fisk <bfisk1711@gmail.com>
2023-10-19 13:25:05 -0500

    Make comment again

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Created on Thu Oct  8 23:29:10 2020
 from util import Util
 from iac_testing_antipatterns import IaCTestingAntipatterns
 
-# small comment
+# small comment number 2
 
 def main():
     
